9f197183d532f771e24229f28f34d92db995f6c9
Nesimi Yarasir <nesimi.yarasir@student.pxl.be>
2024-12-21 10:13:42 +0100

    winkelwagenpagina header aangemaakt en nieuwe css template voor nav

---
+++ JS - Deel 4 - Oefeningen bestanden (1)/oefening1.js
@@ -0,0 +1,14 @@
+const randomTextElements = document.querySelectorAll('.randomtekst');
+console.log(randomTextElements);
+
+const alleImages = document.querySelectorAll('img');
+console.log(alleImages);
+
+const specialeTextElements = document.querySelectorAll('.specialetekst');
+console.log(specialeTextElements);
+
+const fifthParagraph = document.querySelectorAll('p')[4];
+console.log(fifthParagraph);
+
+const RandomText = document.querySelectorAll('.randomtekst')[1];
+console.log(RandomText);
